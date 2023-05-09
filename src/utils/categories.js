a60e4557c20e26e0e62d632a5878c286691d663f
Edith Cantegrit <edith.cantegrit@gmail.com>
2023-05-09 08:04:09 +0200

    add utils

---
+++ src/utils/categories.js
@@ -0,0 +1,12 @@
+export const categories = [
+    'Infraestructuras',
+    'Movilidad',
+    'Medio Ambiente',
+    'Seguridad',
+    'Salud',
+    'Educación',
+    'Cultura',
+    'Acceso a recursos',
+    'Servicio a la ciudadanía',
+    'Otros'
+];
