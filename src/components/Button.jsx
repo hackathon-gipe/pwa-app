2dc394411788c1fa986662e7ec8ec338f45703eb
Edith Cantegrit <edith.cantegrit@gmail.com>
2023-05-09 08:06:12 +0200

    update Button component to link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,28 +1,44 @@
-// Button component with style components, if property primary is passed has #FF7D54 background color and #fff color text if property secondary is passed has #fff background color and #FF7D54 color text. onClick property is required and is a function that will be called when the button is clicked.  The button text is passed as a children property.Also the button has text props to pass a string. the button text is 16px semi bold. the button is 328px width and 50px height.
-
 import React from 'react';
+import { Link } from "react-router-dom";
 import styled from 'styled-components';
+import { colors } from '../styles/colors';
+import { device } from '../styles/breakpoints';
 
-const Button = ({ children, onClick, primary, secondary }) => {
+const Button = ({ children, path, primary, secondary }) => {
     return (
-        <StyledButton onClick={onClick} primary={primary} secondary={secondary}>
-        {children}
+        <StyledButton to={path} $primary={primary} $secondary={secondary}>
+            {children}
         </StyledButton>
     );
 };
 
-const StyledButton = styled.button`
-    background-color: ${props => props.primary ? '#FF7D54' : '#fff'};
-    color: ${props => props.secondary ? '#FF7D54' : '#fff'};
+const StyledButton = styled(Link)`
+    background-color: ${props => props.$primary ? colors.primary : colors.white};
+    color: ${props => props.$secondary ? colors.primary : colors.white};
     font-size: 16px;
+    text-decoration: none;
     font-weight: 600;
-    width: 328px;
-    height: 50px;
-    border: '2px solid #FF7D54';
+    padding: 14px;
+    width: 250px;
+    text-align: center;
+    border: 2px solid ${colors.primary};
     border-radius: 30px;
+    margin-bottom: 20px;
     cursor: pointer;
     &:hover {
         opacity: 0.8;
+    }
+
+    @media ${device.md} {
+        font-size: 18px;
+        padding: 10px;
+        width: 350px;
+    }
+
+    @media ${device.lg} {
+        font-size: 16px;
+        padding: 10px;
+        width: 250px;
     }
 `;
 
