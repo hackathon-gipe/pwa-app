22a12a7f4cb0c8a772dc321b01d5f0abe8c144c0
Edith Cantegrit <edith.cantegrit@gmail.com>
2023-05-09 08:07:17 +0200

    add Paragraph component

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Paragraph = ({ children, size, color }) => {
+    return (
+        <StyledParagraph $size={size} $color={color}>
+            {children}
+        </StyledParagraph>
+    );
+}
+
+const StyledParagraph = styled.p`
+    text-align: center;
+    color: ${props => props.$color};
+    font-weight: 400;
+    margin: 10px;
+     ${props => {
+        if (props.$size === "xs") {
+            return `font-size: 12px;`
+        }
+        if (props.$size === "s") {
+            return `font-size: 12px;`
+        }
+        if (props.$size === "m") {
+            return `font-size: 16px;`
+        }
+        if (props.$size === "l") {
+            return `font-size: 18px;
+            font-weight: 600;
+            `
+        }
+        if (props.$size === "xl") {
+            return `
+            font-size: 22px;
+            font-weight: 600;`
+        } else {
+            return `font-size: 14px;`
+        }
+    }}
+`;
+
+export default Paragraph;
