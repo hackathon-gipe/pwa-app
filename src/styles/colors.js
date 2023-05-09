cc7b7a3799449ce33004f09c0b9cae903a737980
Edith Cantegrit <edith.cantegrit@gmail.com>
2023-05-09 08:03:59 +0200

    add styles

---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    primary : '#FF7D54',
+    secondary: '#BEA7E5', 
+    darkgrey: '#575757',
+    grey: '#9A9A9A',
+    lightgrey: '#DBDBDB',
+    white: '#ffffff',
+    black: '#000000'
+}
