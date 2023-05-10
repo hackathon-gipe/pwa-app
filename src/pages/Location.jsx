872f449bf13a09803af5410974b283e65bbbe465
Edith Cantegrit <edith.cantegrit@gmail.com>
2023-05-10 08:29:04 +0200

    add form into Location screen

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -4,28 +4,108 @@ import styled from 'styled-components';
 import Paragraph from '../components/Paragraph';
 import { colors } from '../styles/colors';
 import { FiMapPin } from 'react-icons/fi';
-import Button from '../components/Button';
+import Input from '../components/Input';
+import { useNavigate } from 'react-router-dom';
+import Dropdown from '../components/Dropdown';
+import { categories } from '../utils/categories';
+import Submit from '../components/Submit';
 
 const Location = () => {
-    const [location, setLocation] = useState(null);
+    const [ location, setLocation ] = useState(null);
+    const navigate = useNavigate();
+    const [dropdownValue, setDropdownValue] = useState("Ámbito");
+    const [errorAPICall, setErrorAPICall] = useState(false);
+
+    const handleSubmit = async (e) => {
+        
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        formData.append("category", dropdownValue);
+
+        const { title, description, category, extraData } = Object.fromEntries(formData.entries());
+        const settings = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(
+            {
+                title, 
+                description, 
+                category, 
+                extraData, 
+                coordinates: {
+                    latitude: location.latitude || '', 
+                    longitude: location.longitude || ''
+                }, 
+                address: {
+                    street: location.street || '', 
+                    city: location.city || '', 
+                    state: location.state || '', 
+                    zip: location.zip || ''
+                }
+            })
+        };
+        try {
+           await fetch(`https://api.ubervo.es/need`, settings);
+            navigate('/success');
+        } catch (e) {
+            setErrorAPICall(true)
+            return e;
+        }
+    }
+
     return (
         <StyledContainer>
-
+             { errorAPICall && 
+                <Paragraph size='s' color='red'>Something went wrong when sending data, try again!</Paragraph>
+            }
             <Map
                 setLocation={setLocation}
             />
             <Paragraph size="xs" color={colors.grey}>Cerca de...</Paragraph>
             <AddressBox>
                 <StyledBox>
-                    <Paragraph size="m" color={colors.grey}>{location && `${location.lat}/`}</Paragraph>
-                    <Paragraph size="m" color={colors.grey}>{location && location.lng}</Paragraph>
+                    { location  && <Paragraph size="s" color={colors.darkgrey}>{location.street || ''} {location.zip || ''} {location.city || ''}</Paragraph>} 
+                    { location && location.state  && <Paragraph size="s" color={colors.darkgrey}>{location.state}</Paragraph>} 
                 </StyledBox>
 
                 <FiMapPin />
             </AddressBox>
-
-            <Button secondary path="/form">Siguiente</Button>
-
+            <StyledForm onSubmit={handleSubmit}>
+                <Input
+                    name="title"
+                    label="Nombre de tu propuesta"
+                    maxLength="50"
+                    minLength="5"
+                    required
+                    placeholder="Resume en una frase ..."
+                />
+                <Input
+                    textarea
+                    name="description"
+                    label="Describe tu propuesta"
+                    maxLength="400"
+                    minLength="10"
+                    required
+                    placeholder="Explica en detalles tu propuesta ..."
+                />
+                <Input
+                    textarea
+                    name="extraData"
+                    label="Especifidades del sitio"
+                    maxLength="100"
+                    placeholder="Si quieres darnos más detalles del lugar..."
+                />
+                 <Dropdown
+                    dropdownValue={dropdownValue}
+                    setDropdownValue={setDropdownValue}
+                    entries={categories}
+                />
+                <Submit secondary>Enviar</Submit>
+            </StyledForm>
         </StyledContainer>
     );
 }
@@ -37,14 +117,24 @@ const StyledContainer = styled.div`
 `;
 
 const StyledBox = styled.div`
-    min-height: 100px;
+   width: 300px;
 `;
 
 const AddressBox = styled.div`
-    width: 70%;
+    min-height: 70px; 
+    width: 100%;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: center ;
+`;
+
+const StyledForm = styled.form`
+    padding-top: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content:   space-between;
+    align-items: center;
+    max-width: 500px;
 `;
 
 
